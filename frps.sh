b1dd0546bbfd5d47c060917e8efce1cffe5a2b0e
Stille <stille@live.com>
2020-02-07 10:53:08 +0800

    #1

--- frps.sh
+++ frps.sh
@@ -4,14 +4,14 @@ REPO="stilleshan/frps.sh"
 WORK_PATH=$(dirname $(readlink -f $0))
 
 # 创建frps文件夹
-mkdir -p /usr/local/frps && \
+mkdir -p /usr/local/frp && \
 # 下载并移动frps文件
 wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 cd frp_${FRP_VERSION}_linux_amd64 && \
-mv frps /usr/local/frps && \
+mv frps /usr/local/frp && \
 # 下载frps.in和frps.service
-wget -P /usr/local/frps https://raw.githubusercontent.com/${REPO}/master/frps.ini && \
+wget -P /usr/local/frp https://raw.githubusercontent.com/${REPO}/master/frps.ini && \
 wget -P /lib/systemd/system https://raw.githubusercontent.com/${REPO}/master/frps.service && \
 systemctl daemon-reload && \
 # 删除多余文件
